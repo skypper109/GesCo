d0d26ff7fc312c3522bb6a53767fe2de214063ae
Mamoutou02 <sangaremoutou.360@gmail.com>
2025-05-08 10:31:03 +0000

    methode voir historique et prochaine tour

--- src/Principale/Agent.java
+++ src/Principale/Agent.java
@@ -12,7 +12,6 @@ import java.util.List;
         private String prenom;
         private String email;
         public AdministrateurRH admin;
-
         public List<Indisponibilite> indisponibiliteList;
         //constructeur
 
@@ -71,7 +70,7 @@ import java.util.List;
             }
             return false;
         }
-
+    // methode voir tour prochaine
         public Historique voirTourProchaine(int idAgent) {
             for (Historique h: admin.historiqueList){
                 if (h.getIdAgent() == idAgent && h.getDateRotation().isAfter(LocalDate.now())){
@@ -79,6 +78,14 @@ import java.util.List;
                 }
             }
             return null;
+        }
+        // methode voir historique
+        public void voirHistorique(int idAgent) {
+            for (Historique h : admin.historiqueList) {
+                if (h.getIdAgent() == idAgent || h.getIdAgentRemp() == idAgent) {
+                    System.out.println("tu as servie le petit dejeuner le : " + h.getDateRotation());
+                }
+            }
         }
     }
 
